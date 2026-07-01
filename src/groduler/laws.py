40f8c360046b3df3ef3f80ab17d5062de7b1ab5b
lwantoch <l.wantoch@icloud.com>
2026-07-01 15:18:32 +0200

    feat(law): Add functionality for fitting Footprint Law

---
+++ src/groduler/laws.py
@@ -0,0 +1,36 @@
+from typing import NamedTuple
+
+import numpy as np
+from scipy.optimize import curve_fit
+
+
+class FootprintFit(NamedTuple):
+    """Fitted constants of the Footpint law.
+
+    Footprint law is the defined as percent of SMactive used by a job on one GPU and is depending of atomsize of the system
+
+       sm_active=(fmax * atom_size)/(factor_k+atom_size)
+    """
+
+    fmax: float
+    factor_k: float
+
+
+def _footprint_law_model(atom_size: np.array, fmax: float, factor_k: float) -> float:
+    """ "defining the model for optimization.
+
+    Function definition for the fit."""
+
+    smact = (fmax * atom_size) / (factor_k + atom_size)
+
+    return smact(np.array)
+
+
+def fitting_law(atom_size: np.array, smact: np.array) -> tuple[float, float]:
+    """Finding the footprint parameters by fitting with SciPy."""
+
+    popt, pcov = curve_fit(_footprint_law_model, atomsize, smact, p0=[50, 500])
+
+    fmax, factor_k = popt
+
+    return fmax, factor_k
