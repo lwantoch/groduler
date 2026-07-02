a0d7319065605cb7a5f7b67c4e618aad420e8732
lwantoch <l.wantoch@icloud.com>
2026-07-02 09:41:36 +0200

    test(laws):  Adding testing for fitting Throughput law

--- tests/test_laws.py
+++ tests/test_laws.py
@@ -1,14 +1,27 @@
 import numpy as np
 from pytest import approx
 
-from groduler.laws import fitting_law
+from groduler.laws import fitting_footprint_law, fitting_throughput_law
 
 
-def test_fitting_law_recovers_known_constants():
+def test_fitting_footprint_law_recovers_known_constants():
 
     atom_size = np.array([2000.0, 5000.0, 20000.0, 120000.0])
     smact = np.array([15.0, 28.0, 54.0, 72.0])
 
-    fmax, factor_k = fitting_law(atom_size, smact)
+    fmax, factor_k = fitting_footprint_law(atom_size, smact)
 
     assert fmax == approx(77.0, abs=1.0)
+
+
+def test_fitting_throughput_law_recovers_known_constants():
+
+    atom_size = np.array([2000.0, 5000.0, 20000.0, 120000.0])
+    throughput = 5000 * (atom_size**20)
+
+    factor_colocation, factor_A, check_r2 = fitting_throughput_law(
+        atom_size, throughput
+    )
+
+    assert factor_A == approx(5000.0, abs=1.0)
+    assert factor_colocation == approx(20.0, abs=1.0)
