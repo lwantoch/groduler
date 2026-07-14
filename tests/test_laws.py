e2d12d04a88b9ae3e6c29e48208c238c41144eef
lwantoch <l.wantoch@icloud.com>
2026-07-14 14:27:10 +0200

    style(tests): remove unused imports

--- tests/test_laws.py
+++ tests/test_laws.py
@@ -1,11 +1,10 @@
 import numpy as np
 from pytest import approx
 
-from groduler.laws import fitting_footprint_law, fitting_throughput_law, optimal_packing
+from groduler.laws import fitting_footprint_law, fitting_throughput_law
 
 
 def test_fitting_footprint_law_recovers_known_constants():
-
     atom_size = np.array([2000.0, 5000.0, 20000.0, 120000.0])
     smact = np.array([15.0, 28.0, 54.0, 72.0])
 
@@ -15,7 +14,6 @@ def test_fitting_footprint_law_recovers_known_constants():
 
 
 def test_fitting_throughput_law_recovers_known_constants():
-
     atom_size = np.array([2000.0, 5000.0, 20000.0, 120000.0])
     throughput = 5000 * (atom_size**20)
 
@@ -26,7 +24,6 @@ def test_fitting_throughput_law_recovers_known_constants():
 
 
 def test_optimal_packing():
-
     system_throughput = [10, 200, 5657, 2836, 56, 909]
     packing_depths = [2, 5, 6, 8, 12, 16]
     best_packing = packing_depths[np.argmax(system_throughput)]
